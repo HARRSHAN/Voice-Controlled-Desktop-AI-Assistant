20c86394a9414e96f675011c2c7e87a2a038581c
HARRSHAN <harrshan4@gmail.com>
2025-07-24 20:49:08 +0530

    Describe what you changed

---
+++ config.py
@@ -0,0 +1,7 @@
+from config import API_KEY
+
+# Then configure your model
+import google.generativeai as genai
+API_KEY = "AIzaSyBbWXlcGVvMTZ0F7IVhCijg5d0RGXM7vVY"
+genai.configure(api_key=API_KEY)
+
